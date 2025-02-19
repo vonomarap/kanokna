ebb005892677c423c078a42efc35b46a124bf9ff
Fed Bauer <alex.massager372@passfwd.com>
2025-02-19 11:57:13 +0300

    refactor: rename UserManagementServiceApplication to UserServiceApplication

--- backend/windows-store-server/services/user-service/src/main/java/com/kanokna/users/userservice/UserServiceApplication.java
+++ backend/windows-store-server/services/user-service/src/main/java/com/kanokna/users/userservice/UserServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserManagementServiceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserManagementServiceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
