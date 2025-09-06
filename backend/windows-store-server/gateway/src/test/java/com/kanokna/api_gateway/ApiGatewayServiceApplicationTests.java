52072b605ff00e380c222620c8199848de65a499
Fed Bauer <alex.massager372@passfwd.com>
2025-09-06 11:21:30 +0300

    feat(gateway): initialize API Gateway service

---
+++ backend/windows-store-server/gateway/src/test/java/com/kanokna/api_gateway/ApiGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanokna.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
